80d3578e3c909408dd50dd8969f5e91f04af2af0
Gantz <ca.palomino.rivera@gmail.com>
2015-06-02 09:08:32 -0500

    First

---
+++ revelacionextrema/PageItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  PageItemViewController.h
+//  revelacionextrema
+//
+//  Created by Cristian Palomino Rivera on 17/05/15.
+//  Copyright (c) 2015 Cristian Palomino Rivera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageItemViewController : UIViewController
+
+@end
---
+++ revelacionextrema/PageItemViewController.m
@@ -0,0 +1,37 @@
+//
+//  PageItemViewController.m
+//  revelacionextrema
+//
+//  Created by Cristian Palomino Rivera on 17/05/15.
+//  Copyright (c) 2015 Cristian Palomino Rivera. All rights reserved.
+//
+
+#import "PageItemViewController.h"
+
+@interface PageItemViewController ()
+
+@end
+
+@implementation PageItemViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
